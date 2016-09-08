84ef0e4ea0dcad6f482f4b7084bf6329ad338fa2
Julia Winchester <Julia.Winchester@stonybrook.edu>
2016-09-08 17:43:15 -0400

    QuizService in progress

--- perspective/perspective-service.js
+++ perspective/perspective-service.js
@@ -1,19 +1,14 @@
 angular.module('app').service('Perspective', Perspective);
 
-Perspective.$inject = ['HTTPService', 'Image', 'Structure', '$timeout'];
+Perspective.$inject = ['HTTPService', 'Image', 'Structure'];
 
-function Perspective(HTTPService, Image, Structure, $timeout) {
+function Perspective(HTTPService, Image, Structure) {
 	var service = {
 		p: {},
 		get: function (perspectiveId, getLabel) {
 			return HTTPService.get('Perspective', getLabel, perspectiveId).then(
 				function (result) {
                     service.set(result[0]);
-  					// service.p = result[0];
-  					// Image.addImage(service.p.data.image, service.p.data.position_x,
-  					// service.p.data.position_y, service.p.data.scale);
-       //              Structure.deleteAllStructs(0);
-       //  			Structure.addStructure(service.p.data.labels);
   			});
 		},
         set: function (pObj) {
@@ -82,4 +77,78 @@ function Perspective(HTTPService, Image, Structure, $timeout) {
         }
 	};
 	return service;
-}	
+}
+
+angular.module('app').service('QuizService', QuizService);
+
+QuizService.$inject = ['Image', 'Structure'];
+
+function QuizService(Image, Structure) {
+    var service = {
+        quizEnabled: false,
+        labelBox: null,
+        labels: [],
+        nCorrect: 0,
+        nCorrectText: null,
+        enable: function () {
+            service.drawQuiz();
+            service.dragOn();
+            view.draw();
+        },
+        drawQuiz: function () {
+            Image.setCanvasWidth(1400);
+            var ctxDims = Image.getCanvasDims();
+            service.labelBox = new Rectangle(950, 50, 1350, ctxDims.height-50);
+            var labelXY; // Write code to give evenly spaced grid locs for n structures
+            for (var i = 0; i < Structure.structures.length; i++) {
+                Structure.structures[i].labelPath.label.visible = false;
+                // Code to draw box on old labels
+                var label = new Text(labelX[i].x, labelY[i].y, 
+                    Structure.structures[i].labelPath.label.text);
+                // Code to draw label borders or background
+                labels.push(label);
+            }
+            service.nCorrectText = new Text(labelBox.bbox.center, 
+                labelBox.bbox.bottom - 40, 
+                nCorrect+'/'+service.labels.length+' correctly placed');
+        },
+        dragOn: function () {
+            service.tool = new Tool(); // After this, add event functions to tool
+
+            var hitOptions = {
+                segments: true,
+                stroke: true,
+                fill: true,
+                tolerance: 5
+            };
+
+            service.tool.onMouseDown = function(event) {
+                project.activeLayer.selected = false;
+                service.tool.item = null;
+
+                var hitResult = project.hitTest(event.point, hitOptions);
+                if (!hitResult || hitResult.item != label || service.labels.findIndex(hitResult.item) == -1) {
+                    return;
+                }
+                service.tool.item = hitResult.item;
+                service.tool.item.box.selected = true;
+                service.tool.mouseStart = event.point;
+            };
+
+            service.tool.onMouseDrag = function(event) {
+                service.tool.item.position = 
+                    service.tool.item.position.add(event.delta);
+                service.tool.item.box.position = 
+                    service.tool.item.box.position.add(event.delta);
+            };
+
+            service.tool.onMouseUp = function(event) {
+                // Where ~the magic~ happens
+            };
+        },
+        disable: function () {
+
+        }
+    };
+    return service;
+}
